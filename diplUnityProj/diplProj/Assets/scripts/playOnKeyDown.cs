0d8b8d9d39cb4a428ebe7ef6ca3830e310b3986d
Dominik Konečný <>
2018-10-16 16:58:28 +0200

    part two

---
+++ diplUnityProj/diplProj/Assets/scripts/playOnKeyDown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class playOnKeyDown : MonoBehaviour {
+
+	AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+		source = this.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if (Input.GetKeyDown (KeyCode.Space))
+			source.Play ();
+	}
+}
